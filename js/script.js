503281720fdac6ee923d97d5d2146a93a64ebb02
sophiegsmith1 <sophiegsmith1@gmail.com>
2026-04-06 19:27:33 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -188,22 +188,19 @@ grid.innerHTML = articles.map(item => `
         <div class="category-label">${item.category}</div>
         
         <div class="card-image">
-            <img src="${BASE}/images/icons/${item.icon || 'default-icon.png'}" 
-                 alt="icon" 
-                 class="card-icon"
-                 onerror="this.src='${BASE}/images/icons/default-icon.png';">
+            <img src="${BASE}/images/icons/${item.icon || 'default-icon.png'}" class="card-icon">
         </div>
 
         <h3>${item.title}</h3>
 
         <div class="media-links">
-            <a href="${item.articleUrl}" target="_blank" class="media-btn article">📄 Article</a>
-            ${item.podcastUrl !== '#' ? `<button class="media-btn podcast" onclick="togglePlayer('audio-${item.id}')">🎙️ Podcast</button>` : ''}
-            ${item.videoUrl !== '#' ? `<a href="${item.videoUrl}" target="_blank" class="media-btn video">📺 Video</a>` : ''}
+            <a href="${item.articleUrl}" target="_blank" class="media-btn">View Article</a>
+            ${item.podcastUrl !== '#' ? `<button class="media-btn podcast" onclick="togglePlayer('audio-${item.id}')">Listen to Podcast</button>` : ''}
+            ${item.videoUrl !== '#' ? `<a href="${item.videoUrl}" target="_blank" class="media-btn video">Watch Video</a>` : ''}
         </div>
 
-        <div id="audio-${item.id}" class="player-container" style="display:none;">
-            <audio controls><source src="${item.podcastUrl}" type="audio/mpeg"></audio>
+        <div id="audio-${item.id}" class="player-container" style="display:none; margin-top:10px;">
+            <audio controls style="width:100%"><source src="${item.podcastUrl}" type="audio/mpeg"></audio>
         </div>
     </div>
 `).join('');
